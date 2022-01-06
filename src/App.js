dce6ab8b80ef93304283c7d18526b623fcda5755
Mentor24 <mk.burdach@gmail.com>
2022-01-07 00:20:10 +0100

    changes_v2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import TOLogo from './images/TOLogo.png'
 import gym from './images/gym.jpg'
+import fbIcon from './images/fbIcon.png'
+import instaIcon from './images/instaIcon.png'
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 
 const S = {
   View: styled.div`
@@ -55,15 +58,42 @@ const S = {
   margin-top: 4%;
   border: solid 1px;
   border-radius: 10px;
+  display: flex;
+  align-items: center;
   `,
 
   Section2: styled.div`
   flex:1;
   width: 85%;
   margin-top: 4%;
-  margin-bottom: 4%
+  margin-bottom: 4%;
   border: solid 1px;
-  border-radius: 10px;;
+  border-radius: 10px;
+  `,
+
+  p1: styled.div`
+  flex:1; 
+  height:100%; 
+  display: flex; 
+  align-items: center; 
+  background-color: green;
+  `,
+
+  p2: styled.div`
+  flex:1; 
+  height:100%; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center;
+  `,
+
+  Section2: styled.div`
+  flex:1;
+  width: 85%;
+  margin-top: 4%;
+  margin-bottom: 4%;
+  border: solid 1px;
+  border-radius: 10px;
   `,
 
   StyledButton: styled(Button)`
@@ -72,7 +102,13 @@ const S = {
   border-radius: 10px;
   background-color: #413fb5;
   font-weight: 700;
-  font-size: 20px; 
+  font-size: 20px;
+  font-family:system-ui; 
+  `,
+
+  StyledTextField: styled(TextField)`
+  width: 100%;
+  height:100%;
   `,
 }
 
@@ -80,21 +116,33 @@ function App() {
   return (
     <S.View>
       <S.Header>
-      <img src={TOLogo} alt="Logo"/>
+      <img src={TOLogo} alt='Logo'/>
       </S.Header>
       <S.Content1>
         <S.Content2>
           <S.Left>
+            <S.Section1>
+              <p>Zarejestruj się jako trener</p>
+            </S.Section1>
+            <S.Section1>
+              <img src={fbIcon} alt='fbIcon'/>
+              <a href='https://www.facebook.com'>Rejestracja przez Facebook</a>
+            </S.Section1>
+            <S.Section1>
+              <img src={instaIcon} alt='instaIcon'/>
+              <a href='https://www.instagram.com'>Rejestracja przez Facebook</a>
+            </S.Section1>
+            <S.Section1>
+              <S.p1></S.p1>
+              <S.p2><p> Lub za pomocą maila</p></S.p2>
+              <S.p1></S.p1>
+            </S.Section1>
             <S.Section1></S.Section1>
             <S.Section1></S.Section1>
             <S.Section1></S.Section1>
             <S.Section1>
-              
+              <S.StyledTextField id="outlined-basic" label="Password..." variant="outlined" />
             </S.Section1>
-            <S.Section1>
-
-            </S.Section1>
-            <S.Section1></S.Section1>
             <S.Section1>
               <S.StyledButton variant="contained">Zarejestruj się za darmo</S.StyledButton>
             </S.Section1>
